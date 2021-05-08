b0612bcec73a9f1aad000413853e5f4b5c82ec57
SquaredLoops <pete@squaredloops.com>
2021-05-08 16:45:49 +0100

    Add welcome message and ESC to close

--- FloatingClock/MainWindow.xaml.cs
+++ FloatingClock/MainWindow.xaml.cs
@@ -10,31 +10,44 @@ namespace FloatingClock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int welcomeIntroCounter = 3;
         readonly System.Timers.Timer _timer = new System.Timers.Timer(1000);
 
         public MainWindow()
         {
             InitializeComponent();
 
-            // Show current date/time before drawing the screen
-            UpdateTimeDisplay();
-
-            _timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            // Call this event on every "tick"
+            _timer.Elapsed += new System.Timers.ElapsedEventHandler(Timer_Elapsed);
             _timer.Enabled = true;
         }
 
-        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            this.Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(UpdateTimeDisplay));
+            //this.Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(UpdateTimeDisplay));
+            UpdateTimeDisplay();
         }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
 
+        private void MainWindow_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+
         private void UpdateTimeDisplay()
         {
+            // Show welcome message for a few ticks
+            if (welcomeIntroCounter-- > 0) { return; }
+            welcomeIntroCounter = 0;
+
+            // Update the date and time
             this.Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
             {
                 DigialTime.Content = $"{DateTime.Now:HH:mm}";
